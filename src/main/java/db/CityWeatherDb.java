f509f059bc07f7df7c3e4451a228c122ab128b01
Grzegorz Piotrowski <gpiotrowskiarch@gmail.com>
2023-11-21 18:51:53 +0100

    simple impl

--- src/main/java/db/CityWeatherDb.java
+++ src/main/java/db/CityWeatherDb.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 public class CityWeatherDb {
 
-    private static final Map<String, CityDataEntity> dataBase = new HashMap<>();
+    private static final Map<Long, CityDataEntity> dataBase = new HashMap<>();
 
     // TODO: metody do pracy nad bazą danych, dodawanie itp...
-
+    public void add(CityDataEntity entity) {
+        dataBase.put(entity.getId(), entity);
+    }
 }
