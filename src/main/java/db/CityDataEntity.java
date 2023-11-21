f800e9a63001e7a10aa86c54e8e787a784e32628
Grzegorz Piotrowski <gpiotrowskiarch@gmail.com>
2023-11-21 18:16:42 +0100

    simple impl

--- src/main/java/db/CityDataEntity.java
+++ src/main/java/db/CityDataEntity.java
@@ -4,4 +4,28 @@ public class CityDataEntity {
     private Long id;
     private String name;
     private WeatherDataEntity weatherDataEntity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WeatherDataEntity getWeatherDataEntity() {
+        return weatherDataEntity;
+    }
+
+    public void setWeatherDataEntity(WeatherDataEntity weatherDataEntity) {
+        this.weatherDataEntity = weatherDataEntity;
+    }
 }
